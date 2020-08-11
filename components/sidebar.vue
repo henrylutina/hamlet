8c022c3335073d6c78eb1e1615ad95a32eca0dd5
MmonimUkpanah <mukpanah22@gmail.com>
2020-08-11 13:12:50 +0100

    added navbar and sidebar components

---
+++ components/sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="one">
+        <div class="one1">
+            <img src="~/assets/icon.png" alt="">
+            
+            <h5>HIRING CHECKLIST</h5>
+            <p>Contact Info</p>
+            <p>Job Details</p>
+            <p>Offer Details</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: 'Overpass', sans-serif;
+    }
+    .one{
+        top: 0;
+        left: 0;
+        width: 25%;
+    }
+    .one1{
+        padding-left: 100px;
+        margin-top: 50px;
+        padding-right: 10px;
+    }
+    .one1 img{
+        width: 60%;
+        vertical-align: middle;
+        border-radius: 50%;
+        margin-bottom: 20px;
+    }
+    .one1 h5{
+        margin-bottom: 10px;
+    }
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'font', href: 'href="https://fonts.googleapis.com/css2?family=Overpass&display=swap" rel="stylesheet"'}
     ]
   },
   /*
